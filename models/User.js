a212c96569af8bfdb1ec276c007ab3f15fffc268
Salieu Sarr <95061054+Ssrr17@users.noreply.github.com>
2022-05-05 14:24:11 -0400

    folders and user model started

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    userName:{
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+
+    },  
+     email:{
+        type: String,
+        required: true,
+        unique: true,
+        match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/]
+     },
+     thoughts:[{
+         type: Schema.Types.ObjectId,
+         ref: 'Thoughts'
+     }],
+     friends:[{
+         type: Schema.Types.ObjectId,
+        ref: 'User'
+     }]
+
+});
+
+
+module.exports = UserSchema
